96395f966ad64b438344e7fd4f171f662c7fc4ae
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-01-31 15:25:15 -0700

    create board

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static char[,] gameBoard = new char[3, 3]; // 3x3 game board
+
+    static void Main(string[] args)
+    {
+        // welcome user to the game
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        // create game board array
+        CreateBoard(gameBoard);
+
+        // make current player
+        char currentPlayer = 'X';
+    }
+
+    // method that creates and initializes the game board
+    static void CreateBoard(char[,] board)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                board[row, col] = ' ';
+            }
+        }
+    }
+
+    // method to get player choice
+    static void GetPlayerChoice(char currentPlayer)
+    {
+
+    }
+}
