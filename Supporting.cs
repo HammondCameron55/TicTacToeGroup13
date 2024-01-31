ac0d715cee8523fc8f7783d92c2e99af0be9e2f6
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-01-31 15:48:06 -0700

    method names

--- Supporting.cs
+++ Supporting.cs
@@ -8,6 +8,16 @@ namespace TicTacToeGroup13
 {
     internal class Supporting
     {
+        // method to print board based on information
+        static void gameBoardPrint()
+        {
 
+        }
+
+        // method to receive game board array as input and return if there was a winner
+        static void checkWinner()
+        {
+
+        }
     }
 }
