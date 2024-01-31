78750c9fd165a91043d7003b67d5a7981eb11ba0
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-01-31 16:17:32 -0700

    Finished our (Cam and Jimmy) end

--- Program.cs
+++ Program.cs
@@ -1,85 +1,85 @@
 ﻿using System;
 using TicTacToeGroup13;
-//Make new instance of supporting class
-Supporting sup = new Supporting();
 
-char[,] gameBoard = new char[3, 3]; // 3x3 game board
+class TicTacToeGame
+{
+    static char[,] gameBoard = new char[3, 3]; // 3x3 game board
 
-
-static void Main(string[] args)
+    static void Main(string[] args)
     {
-        
-
-        bool gameOver = false;
-
-        // welcome user to the game
-        Console.WriteLine("Welcome to Tic-Tac-Toe!");
-
-        // create game board array
-        CreateBoard(gameBoard);
-
-        do
-        {
-            // make current player
-            char currentPlayer = 'X';
-
-            
-
-            //Ask each player for their choice
-            GetPlayerChoice(char currentPlayer);
-
-            //Print the board
-            Console.Write(sup.gameBoardPrint);
+        Supporting sup = new Supporting();
 
 
-            if (sup.checkWinner() == true)
-            {
-                gameOver = true;
-            }
-
-        } while (gameOver = false);
-        
-
-        
-
-        //Check for a winner
-        
-
-
-    }
-
-    // method that creates and initializes the game board
-    static void CreateBoard(char[,] board)
-    {
+        // Initialize game board
         for (int row = 0; row < 3; row++)
         {
             for (int col = 0; col < 3; col++)
             {
-                board[row, col] = ' ';
+                gameBoard[row, col] = ' ';
             }
+        }
+
+        bool gameOver = false;
+        char currentPlayer = 'X';
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        while (!gameOver)
+        {
+            Console.WriteLine("Current Player: " + currentPlayer);
+            GetPlayerChoice(currentPlayer);
+
+            
+            
+
+            Console.Write(sup.gameBoardPrint(gameBoard)); // Assuming gameBoardPrint method takes gameBoard as an argument
+
+            gameOver = sup.checkWinner(gameBoard); // Assuming checkWinner method takes gameBoard as an argument
+
+            if (sup.checkWinner(gameBoard))
+            {
+                gameOver = true;
+                Console.WriteLine($"Player {currentPlayer} wins!");
+            }
+            else
+            {
+                if (currentPlayer == 'X')
+                {
+                    currentPlayer = 'O';
+                }
+                else
+                {
+                    currentPlayer = 'X';
+                }
+            }
+
+            
         }
     }
 
-    // method to get player choice
+    // Method to get player choice and update the game board
     static void GetPlayerChoice(char currentPlayer)
     {
-        int across = 0;
-        int down = 0;
-        Console.WriteLine("Where would you like to place your mark? Enter the corresponding coordinates:");
-        Console.WriteLine("ex. 0,0  would place your mark at the top left and 2,2 would place your mark in the bottom right");
-        Console.WriteLine("Please enter the horizontal coordinate: 0, 1, or 2");
-        across = int.Parse(Console.ReadLine());
+        int across, down;
+        bool validInput = false;
 
-        Console.WriteLine("Please enter the vertical coordinate: 0, 1, or 2");
-        down = int.Parse(Console.ReadLine());
-
-        if (gameBoard[across, down] == 'X' || gameBoard[across, down] == 'O')
+        while (!validInput)
         {
-            Console.WriteLine("This position is already occupied. Please choose another position.");
-        }
-        else
-        {
-            gameBoard[across, down] = currentPlayer;
-        }
-    
+            Console.WriteLine("Where would you like to place your mark? Enter the corresponding coordinates:");
+            Console.WriteLine("ex. 0,0  would place your mark at the top left and 2,2 would place your mark in the bottom right");
+            Console.WriteLine("Please enter the horizontal coordinate: 0, 1, or 2");
+            across = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the vertical coordinate: 0, 1, or 2");
+            down = int.Parse(Console.ReadLine());
 
+            if (gameBoard[across, down] == ' ')
+            {
+                gameBoard[across, down] = currentPlayer;
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("This position is already occupied. Please choose another position.");
+            }
+        }
+    }
+}
--- Supporting.cs
+++ Supporting.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+Program p = new Program();
+
 namespace TicTacToeGroup13
 {
     internal class Supporting
     {
         // method to print board based on information
-        static public void gameBoardPrint()
+        static public void gameBoardPrint(p.gameBoard)
         {
 
         }
 
         // method to receive game board array as input and return if there was a winner
-        static public void checkWinner()
+        static public void checkWinner(p.gameBoard)
         {
 
         }
